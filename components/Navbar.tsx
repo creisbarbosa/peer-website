cfd487b69f28fa43bab2ff0868e8bec84ffc2fd3
Cassio Reis Barbosa <creisbarbosa@gmail.com>
2022-12-13 11:34:42 -0300

    Tailwind config colors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,19 @@
 export default function Navbar() {
   return (
-    <nav className="flex sm:justify-center space-x-4 m-2">
-      {[
-        ['Home', '/dashboard'],
-        ['Compre', '/buy'],
-        ['Seja Parceiro', '/partner'],
-        ['Recursos', '/resources'],
-        ['A Peerdustry', '/about'],
-      ].map(([title, url]) => (
-        <a href={url} className="rounded-lg px-3 py-2 text-blue-900 font-medium hover:bg-slate-100 hover:text-slate-900" key="">{title}</a>
-      ))}
+    <nav className="flex sm:justify-center bg-slate-200 border-solid border-b border-stroke-40">
+      <div className="flex sm:justify-center">
+        {[
+          ['Home', '/dashboard'],
+          ['Compre', '/buy'],
+          ['Seja Parceiro', '/partner'],
+          ['Recursos', '/resources'],
+          ['A Peerdustry', '/about'],
+        ].map(([title, url]) => (
+          <a href={url} className="px-6 py-6 text-blue-500 font-regular hover:bg-slate-100" key="">{title}</a>
+        ))}
+      </div>
+      <button className="flex px-2 py-3 bg-blue-50 border-solid border-stroke-40">Entrar</button>
+      <button>Falar com nossos consultores</button>
     </nav>
   )
 }
