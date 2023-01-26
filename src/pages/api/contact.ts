7ece183c59699f6dfb502bb835be240141c3ad9d
Cassio Reis Barbosa <creisbarbosa@gmail.com>
2023-01-26 18:41:46 -0300

    Contact form API integrated

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,29 +1,36 @@
-export default function Mailer (req, res) {
-  require('dotenv').config()
+export default async function Sendingblue( req, res) {
+
+  const SibApiV3Sdk = require('@sendinblue/client')
   
-  let nodemailer = require('nodemailer')
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-      user: 'demo email',
-      pass: process.env.password,
-    },
-    secure: true,
-  })
-  const mailData = {
-    from: 'demo email',
-    to: 'your email',
-    subject: `Message From ${req.body.name}`,
-    text: req.body.message + " | Sent from: " + req.body.email,
-    html: `<div>${req.body.message}</div><p>Sent from:
-    ${req.body.email}</p>`
-  }
-  transporter.sendMail(mailData, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info)
-  })
-  res.status(200)
+  let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+  
+  require('dotenv').config({path: "./vars/.env"})
+  
+  let apiKey = apiInstance.authentications['apiKey'];
+  apiKey.apiKey = process.env.API_KEY;
+  
+  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+  sendSmtpEmail.subject = "Mensagem de Contato Website Peerdustry";
+  sendSmtpEmail.htmlContent = "<html> <body> <h1>Mensagem de contato de {{params.name}}</h1> <h2><b>Empresa:</b> {{params.company}}</h2> <p><b>Mensagem:</b> {{params.message}}</p> <p><b>Telefone de contato:<b/> {{params.phone}}</p> <p>Email de contato: {{params.email}}</p> </body> </html>";
+  sendSmtpEmail.sender = {email: 'cassio.reis@peerdustry.com', name: 'Contato Website Peerdustry'};
+  sendSmtpEmail.to = [{email: 'cassio.reis@peerdustry.com', name: 'Cassio Reis'}];
+  sendSmtpEmail.replyTo = {"email":"replyto@peerdustry.com"};
+  sendSmtpEmail.headers = {"Some-Custom-Name":"unique-id-1234"};
+  sendSmtpEmail.params = {"name":`${req.body.name}`,"email":`${req.body.email}`,"company":`${req.body.company}`,"phone":`${req.body.phone}`,"message":`${req.body.message}`,}
+  
+  await apiInstance.sendTransacEmail(sendSmtpEmail)
+    .then(function(data) {
+      return new Promise<void>((resolve, reject) => {
+        res.statusCode = 201
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Cache-Control', 'max-age=180000');
+        res.end(JSON.stringify(data));
+        resolve();
+      })
+
+    }, 
+      function(error) {
+        console.error(error);
+  });
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -10,7 +10,7 @@ export default function Contact () {
   const [ message, setMessage ] = useState('')
   const [ submitted, setSubmitted ] = useState(false)
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault()
     console.log('Enviando')
 
@@ -22,26 +22,34 @@ export default function Contact () {
       message,
     }
 
-    fetch('api/contact', {
+    const options = {
       method: 'POST',
       headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
+        accept: 
+          'application/json',
+          'content-type': 'application/json',
       },
       body: JSON.stringify(data)
-    }).then(response => {
-      console.log('Resposta recebida')
-      if (response.status === 200) {
-        console.log('Resposta bem sucedida!')
-        setSubmitted(true)
-        setName('')
-        setEmail('')
-        setCompany('')
-        setPhone('')
-        setMessage('')
-      }
-    })
-  }
+    };
+    
+    await fetch('api/contact', options)
+      .then((res) => {
+        console.log('Response received')
+        console.log(res.status)
+        res.json()
+        if (res.status === 201) {
+          alert('Sua mensagem foi enviada com sucesso e em breve nossa equipe entrará em contato! Obrigado!')
+          setSubmitted(true)
+          setName('')
+          setEmail('')
+          setCompany('')
+          setPhone('')
+          setMessage('')
+
+        }
+      })
+      .catch(err => console.error(err));
+    }
 
   return (
     <div className="h-[88vh] bg-blue-50 flex flex-row px-2 lg:px-32 xl:px-64">
@@ -57,7 +65,6 @@ export default function Contact () {
         <Image priority src={Illustration} alt="" className="w-3/4"/>
       </section>
       <section className=" bg-blue-50/50 basis-1/2 h-full relative flex items-center justify-center">
-        {/* <Image src={Background} alt="" className="h-full w-full object-cover absolute"/> */}
         <form className="relative w-96 flex flex-col gap-4">
         
           <div 
@@ -71,8 +78,9 @@ export default function Contact () {
               type="text"
               name="name"
               placeholder="Seu Nome"
-              required
+              required={true}
               className="rounded-md px-4 py-2 border-solid border border-blue-200/60 placeholder-blue-200 text-sm text-blue-400 focus:outline-blue-200"
+              value={name}
               onChange={event => {
                 setName(event.target.value)
               }}
@@ -90,8 +98,9 @@ export default function Contact () {
               type="email"
               name="email"
               placeholder="seuemail@empresa.com"
-              required
+              required={true}
               className="rounded-md px-4 py-2 border-solid border border-blue-200/60 placeholder-blue-200 text-sm text-blue-400 focus:outline-blue-200"
+              value={email}
               onChange={event => {
                 setEmail(event.target.value)
               }}
@@ -109,8 +118,9 @@ export default function Contact () {
               type="text"
               name="company"
               placeholder="Nome da empresa"
-              required
+              required={true}
               className="rounded-md px-4 py-2 border-solid border border-blue-200/60 placeholder-blue-200 text-sm text-blue-400 focus:outline-blue-200"
+              value={company}
               onChange={event => {
                 setCompany(event.target.value)
               }}
@@ -129,6 +139,7 @@ export default function Contact () {
               name="phone"
               placeholder="(00) 0000 - 0000"
               className="rounded-md px-4 py-2 border-solid border border-blue-200/60 placeholder-blue-200 text-sm text-blue-400 focus:outline-blue-200"
+              value={phone}
               onChange={event => {
                 setPhone(event.target.value)
               }}
@@ -140,6 +151,7 @@ export default function Contact () {
             placeholder="Digite aqui sua  mensagem" 
             required
             className="rounded-md border border-solid border-blue-200/60 placeholder-blue-200 text-sm text-blue-400 px-4 py-2 resize-none h-24 focus:outline-blue-200"
+            value={message}
             onChange={event => {
               setMessage(event.target.value)
             }}
@@ -148,7 +160,7 @@ export default function Contact () {
           <input
             type="submit"
             value="enviar"
-            className="bg-blue-500 py-2 text-blue-100 font-bold uppercase rounded-md border border-solid border-blue-400 hover:bg-red-600 hover:border-red-700 hover:text-red-50 transition-colors"
+            className="bg-blue-500 py-2 text-blue-100 font-bold uppercase rounded-md border border-solid border-blue-400 hover:bg-red-600 hover:border-red-700 hover:text-red-50 transition-colors cursor-pointer"
             onClick={event => {
               handleSubmit(event)
             }}
